a3d55c3dd5202f90e3c2810ec5baec60024eaa6f
Francesco Varani <francovaro@gmail.com>
2021-10-18 19:29:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# f446re_sd_card
+ Using a SPI SD card reader with a STM32F466RE Nucleo board
---
+++ src/main.c
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Francesco Varani
+  * @version V1.0
+  * @date    18/10/2021
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+#include "stm32f4xx.h"
+#include "spi.h"
+			
+/**
+ *
+ * @return
+ */
+int main(void)
+{
+	SPI_Config();
+
+	while(1)
+	{
+
+	}
+}
